cb7cc27d96ac2b0a4750ca7fd041365ff2a33419
Martyn Egan <87206678+MartynEgan@users.noreply.github.com>
2021-09-15 18:10:44 +0100

    add scripts for Tuesday and Wednesday

---
+++ Tuesday script.R
@@ -0,0 +1,334 @@
+########################
+# ASDS Code Camp Day 2 #
+########################
+# R basics #
+############
+
+#The hash sign "#" is used to comment out text
+#Text preceded by "#" will not be read by R
+
+This will be read by R, and will produce an error message
+
+####################
+# First things first
+####################
+
+#An R session should begin as an empty space, i.e. there
+#should be no *objects* in our environment. 
+
+ls()
+
+#The ls() *function* shows us a *list* of objects in 
+#our environment.
+
+#Let's make an object. To make an object, we need a
+#name (here "x"), and some value(s) to assign to it.
+
+x <- "Hello world"
+x
+ls()
+
+#One of R's strengths is the large number of *packages*
+#which users have created for different tasks. Packages
+#contain functions which we can use on our data.
+
+search()
+
+#The search() function shows us which packages are loaded
+#in our R session. 
+
+install.packages("tidyverse")
+
+#The install.packages() function allows us to install new
+#packages from the web. Here, we are installing a suite
+#of packages known as the "tidyverse".
+
+library("tidyverse")
+
+#Installing a package does not make it available to us in
+#our R session. To make a package available, along with
+#its functions, we need to use the library() function.
+
+search()
+
+#The tidyverse packages are now listed in our session.
+
+#######
+# Help!
+#######
+
+#There are many ways of accessing help in R. The help()
+#function is the main method. Its shortcut is "?". 
+
+help(tidyverse)
+?tidyverse
+
+?persp
+example(persp)
+
+#The example() function provides an interactive demo
+
+?dplyr
+vignette("dplyr")
+browseVignettes(package = "dplyr")
+
+#Some packages have *vignettes*, which go into more 
+#detail than R help files (which can be very terse). 
+#The vignette() function accesses these, or use
+#browseVignettes(package = "name") to search.
+
+help.search("standard deviation")
+
+#If you're looking for help on a particular area, and
+#aren't sure what function or package to use, try the
+#help.search() function. 
+
+######
+# OOPs
+######
+
+#R is an *object-oriented programming language*. This 
+#means, essentially, that we make objects with functions
+#which we then manipulate with other functions.
+
+rm(list = ls())
+
+#The rm() function can be used to *remove* objects. Here,
+#we are passing the contents of the *list* created by
+#calling the ls() function which we used earlier. Do
+#not worry if this is unclear for now: we are simply 
+#clearing our workspace.
+
+x <- "Hello"
+class(x)
+length(x)
+
+#Here, we assign the value "Hello" to the object "x". 
+#The class() function tells us what *class* of object 
+#we have just created, in this case an object of 
+#class *character*. The length() function tells us how 
+#many discrete values or elements the object contains. 
+#Here, we have length = 1. Is this the result you were 
+#expecting?
+
+x[2] <- "world"
+x
+length(x)
+str_length(x)
+
+#We can use square brackets [] after an object's name to
+#access a particular element by its *index*. Indexes in R
+#begin at 1. x[1] is therefore "Hello". We can use [] 
+#together with the *assignment operator* <- to add an 
+#element to x, here the word "world", which will be stored
+#in the second index position. We now have a *character 
+#vector* of length 2. The str_length() function returns
+#the number of characters within each discrete element of
+#x, here 5, 5 for [1] Hello, and [2] world.
+
+x <- rnorm(n = 50)
+length(x)
+mean(x)
+sd(x)
+
+#We can use functions to create objects. Here, we use
+#rnorm() to create an object comprising 50 *random* 
+#numbers drawn from the *normal distribution* family
+#(here with a mean of 0 and standard deviation of 1: 
+#these are the default settings for the function).
+
+y <- x
+y <- 2*y
+lm(y~x)
+
+#A strength of R is that many functions will be applied
+#automatically to every element in an object. Here, the
+#multiplication function * takes the *scalar* input 2 
+#and applies it to each discrete element in y. We then
+#simultaneously assign the result of this operation back
+#onto y using the assignment operator.
+
+xylm <- lm(y~x)
+xylm
+attributes(xylm)
+summary(xylm)
+plot(x,y)
+
+#Many functions return large *lists* of objects. Here, 
+#we use the lm() function to apply a linear regression
+#of the objects y and x on each other, and assign the 
+#results to a new object, xylm. We use the attributes()
+#function to list the objects created by lm(). We use
+#the summary() function to provide an output of summary
+#statistics. Unsurprisingly, we discover that the slope
+#of y is 2: when we plot() x and y, they are perfectly
+#linear. R provides a helpful warning that this is the
+#case in the output of summary().
+
+xylm$residuals
+plot(xylm$residuals)
+
+#One of the objects which lm() creates is the value of 
+#the *residuals* (i.e. the values "left over" between
+#the predicted and actual values of y). We can access 
+#these by using the "$" operator. When a function creates
+#a *list* of objects, passing the name of the "top level"
+#object (here, xylm) followed by $ and then the "lower
+#level" object allows us to access the latter. 
+
+###########
+# FUNctions
+###########
+
+#As well as being an object-oriented language, R is also
+#a *functional* language. This essentially means that,
+#rather than constantly writing loops in our code, we can
+#express iterative behaviour implicitly. 
+
+x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+#Here is the long way of creating an object comprising
+#a vector of the numbers 1 to 10.
+
+x <- vector("double", 10)
+for(i in 1:10){
+  x[i] <- i
+}  
+
+#Here is a loop doing the same thing.
+  
+x <- seq(1:10)
+
+#And here is the seq() function doing the same: a large
+#saving in mental effort.
+
+y <- x + 11
+y <- c(x, 11)
+
+#Be careful though: here, if we want to add the number
+#11 onto the end of our sequence, we must use the c()
+#function. If we "add" using the + function, R will
+#add 11 to each discrete element in x.
+
+?"+"
+
+?"c"
+
+?"<-"
+
+#Every operator is itself a function, as we see by
+#calling help on the operator within quote marks.
+
+xMean <- mean(x)
+xLength <- length(x)
+xDev <- x - xMean
+xDevSq <- xDev^2
+xSumDevSq <- sum(xDevSq)
+xVar <- xSumDevSq/xLength
+xSD <- sqrt(xVar)
+
+#Here, we use a number of different functions separately
+#to calculate the standard deviation of our sequence, x.
+#Does this tally with the result we obtain by calling
+#the sd() function?
+
+sd(x)
+?sd
+
+#Caution: sd() (and the var() function) assume you are 
+#using *sample* statistics, for which the denominator
+#is n-1. 
+
+xVar_s <- xSumDevSq/(xLength - 1)
+XSD_s <- sqrt(xVar_s)
+sd(x)
+
+sd_p <- function(x){
+  sd(x)*sqrt((length(x)-1)/length(x))
+}
+
+sd_p(x)
+
+#By playing around a little with existing functions, R
+#allows us to create a new function which calculates 
+#standard deviation for a population. Do not worry about
+#what's going on here: the aim is simply to show you how
+#using objects and functions allows us to condense our
+#workload massively.
+
+#####################
+# Taking R for a spin
+#####################
+
+#Let's finish by seeing a little of what R is capable of.
+
+?mtcars
+summary(mtcars)
+str(mtcars)
+head(mtcars)
+ls.str(mtcars)
+
+#mtcars is a built-in dataset contained in R's datasets
+#library. It is, unsurprisingly, a dataset about cars. 
+#The summary() function provides some summary statistics
+#of the dataset. The str() function provides information
+#about the *structure* of the dataset. The head() function
+#provides the first 6 entries in each variable. The 
+#ls.str() function combines the ls() and str() functions.
+#These are all useful functions to remember when
+#exploring a new dataset.
+
+mtcars$am <- as.factor(mtcars$am)
+mtcars$cyl <- as.factor(mtcars$cyl)
+
+ggplot(mtcars, aes(wt, mpg, size = hp)) +
+  geom_text(aes(size = hp, label = cyl, color = am)) +
+  geom_smooth(aes(linetype = cyl), color = "grey", size = 0.5, se = FALSE, show.legend = FALSE) +
+  guides(size = "none") +
+  theme_classic() +
+  theme(legend.title = element_blank(), legend.justification = c(1, 1), legend.position = c(1, 1)) +
+  scale_color_manual(labels = c("manual", "automatic"), values = c("blue", "red")) +
+  labs(title = "Plot of Fuel Efficiency by Weight for 32 Cars", subtitle = "Number of cylinders; size = horsepower") +
+  xlab("weight (1000 lbs)")
+
+#The above example makes use of the ggplot package (a 
+#more advanced graphics package than base R graphics)
+#to produce a scatter plot which is able simultaneously
+#to show the relationship between five variables: the
+#weight of the car, its fuel economy, the number of 
+#cylinders, its horsepower and type of transmission. 
+#Further, three trend lines show the relationship 
+#between weight and mpg for 4, 6 and 8 cylinder cars. 
+#It is not always a good idea to show so many variables
+#simultaneously, but the plot gives a good idea of what
+#is possible with R. Again, do not worry about the 
+#specifics of the code at this point.
+
+##########
+# Activity
+##########
+
+#The mpg dataset is a built in dataset for the ggplot
+#package. By recycling the code in this script file, 
+#explore the dataset and try creating your own simple 
+#plots of the variables.
+
+help(mpg)
+head(mpg)
+
+#plot(mpg$xvar, mpg$yvar)
+
+#ggplot(mpg, aes(xvar, yvar)) +
+  #geom_point()
+  #geom_line()
+  #geom_hist()  #note: takes only 1 continous xvar as an argument in ggplot()
+
+
+################
+# And finally...
+################
+
+#While it might feel a bit overwhelming at first, using
+#R gets easier with time and practice. Well done though 
+#for getting through your first R session, and 
+#congratulations, you survived R basics! 
---
+++ Wednesday script.py
@@ -0,0 +1,303 @@
+########################
+# ASDS Code Camp Day 3 #
+########################
+# Python basics #
+#################
+
+#The hash sign "#" is used to comment out text
+#Text preceded by "#" will not be read by Python
+
+This will be read by Python, and will produce an error message
+
+####################
+# First things first
+####################
+
+#There is no equivalent in Python to R's ls() function. 
+
+dir()
+
+#Calling dir() in Python is similar to calling ls() and search() 
+#simultaneously in R: it provides a list of names of objects in the 
+#current *scope*, which is similar to R's concept of environment.
+
+dir?
+
+#Unlike R, there isn't a clear distinction between objects *you* 
+#create and Python's internal objects. In fact, each time you ask
+#Python to interpret a line of code in the console, it saves that
+#line as an object. 
+
+_1
+dir(_1)
+
+#Fortunately, spyder has a variable explorer, which helps keep
+#things tidy. You can see variables in the window, and import or
+#remove variables.
+
+a = "Hello World"
+print(a)
+
+#The core distribution of Python has very limited data science 
+#capabilities. For this reason, doing data analysis in Python 
+#requires additional libraries to be attached.
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#The three most important of these libraries are numpy (short for
+#Numerical Python), which provides some additional data structures
+#for arrays; pandas (short for Panel Data), which does the same for 
+#tabular data; and matplotlib, which provides graphical capabilities.
+#Anaconda comes with all three installed, and to use them in a 
+#Python session we *import* them *as* an abbreviated version. 
+
+conda update package_name
+
+#To update a package in Anaconda we use the above command.
+
+conda install package_name
+
+#To install a new package in Anaconda we use the above command.
+
+#######
+# Help!
+#######
+
+help()
+help?
+?help
+
+#Accessing help in Python is similar to R. Calling the help() 
+#function, and passing the name of a function within the brackets,
+#will call the help file for that function. It is also generally 
+#possible to use a question mark (?) as a shortcut, though be 
+#careful as this is not a "built-in" capability.
+
+len(a)
+len?
+help(len)
+
+help(pd)
+
+#Here we call help on a built-in function, len(), which functions
+#like length() in R - or does it? When we pass our recently created
+#object "a" to the length() function, what value does it return?
+
+
+#############
+# Making data
+#############
+
+#Mostly in Python you will be dealing with data which you have
+#imported from external sources, or data types associated with 
+#numpy or pandas. However, it is good to know about Python's basic 
+#data structures, because everything else is built on them, and they
+#can be less intuitive to work with than R.
+
+b = 5
+print(b)
+
+#In R, the assignment operator is <-. In Python, we use the equals
+#sign (=). (We can also use = in R, but it is discouraged to do so
+#as it behaves slightly differently to <- in some circumstances).
+
+c = [1, 2, 3]
+print(c)
+
+d = [1, "two", 3]
+print(d)
+
+#Python has numerous types of data structures: "a" is a string (str); 
+#"b" is an integer (int), which are both types of *scalar*, or 
+#single values; "c" and "d" are both *lists*, which contain multiple 
+#values. Note the use of square brackets [] and comma separation of 
+#values. Note also that a list can contain both integers and strings.
+
+e = 1, 2, 3
+print(e)
+
+f = (1, 2, 3)
+print(f)
+
+#A *tuple* is similar to a list, but its contents cannot be modified.
+#Note that round brackets () are not needed, but can be used.
+
+g = {1, 2, 3, 4}
+print(g)
+
+h = {1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 4}
+print(h)
+
+i = {1: 'This', 2: 'is', 3: 'a', 4: 'dict'}
+print(i)
+
+#The last two basic structures are sets and dicts. A set is like a 
+#list, but can only contain unique elements: here, "g" and "h" are 
+#identical. Sets are created using curly braces {}. A dict is an 
+#important data structure in python. Similar to a set, it is created
+#using curly braces, but it contains pairs of *keys* and *values*, 
+#separated by colons. 
+
+data = {'year': [1992, 1997, 2002, 2007, 2011, 2016, 2020],
+        'party': ['Fianna Fail', 'Fianna Fail', 'Fianna Fail', 'Fianna Fail', 'Fine Gael', 'Fine Gael', 'Fianna Fail'],
+        'seats': [68, 77, 81, 78, 76, 50, 38]}
+print(data)
+type(data)
+
+data_frame = pd.DataFrame(data)
+print(data_frame)
+
+#The above example gives an idea of how data science libraries such
+#as pandas build on basic Python data structures. Here, we create a 
+#dict comprising three lists, each defined by a different key. If 
+#we then pass this dict to the DataFrame() function in pandas 
+#(remembering to use the pd. alias when calling the function) we 
+#can tranform the dict into a dataframe: a much more useful data
+#structure for data analysis.
+
+###########
+# Functions
+###########
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = pd.DataFrame(a)
+a.mean()
+
+#We were introduced to functions in the section on help. An 
+#important difference between Python and R is the concept of 
+#*methods*. In R, every function is called the same way: the name
+#of the function is followed by round brackets, and the *arguments*
+#of the function are placed inside the brackets. Many Python 
+#functions operate the same way. However, *methods* operate slightly
+#differently. A method is a function which belongs to a specific
+#*class* of object, and it is called by placing a dot (.) after the
+#name of the object, followed by the method, followed by round 
+#brackets. Here, we make a list of numbers, "a", which we then 
+#convert into a dataframe, an object belonging to the pandas module,
+#using the function DataFrame() and the pd alias. We then call the 
+#mean() *method*, a type of function belonging to the dataframe 
+#class, on this object. 
+
+?mean
+?mean()
+?.mean()
+help(mean)
+
+?a.mean
+
+#To call the help file on a method, we need to supply an object of 
+#the proper class. Here, for example, the first four calls to help 
+#on the .mean() method do not work. Calling help on methods can be 
+#extremely unintuitive, and is a good example of Python's steep 
+#learning curve. 
+
+data_frame.seats.mean()
+data_frame['seats'].mean()
+
+#Here, we call the .mean() method on the 'seats' column of the 
+#data_frame object we just created. Notice that to select the seats 
+#column we can use either a dot (.) after the name of our object, 
+#or square brackets [] with the column name inside, surrounded by 
+#quotes ''. Note how this differs to R's $ operator. We then call 
+#the mean() method by adding another dot(.), followed by round 
+#brackets. 
+
+data_frame['seats'].groupby(data_frame['party']).mean()
+data_frame['seats'].groupby(data_frame['party']).std()
+
+#It is possible to chain methods together to perform fairly complex
+#operations in a very concise manner. Say we wanted to calculate
+#the average number of seats won by the largest party in the Dail
+#following a general election, and to see whether there is any 
+#difference between the average number of seats won by Fianna Fail
+#and Fine Gail. To achieve this, we first select our object and 
+#column of interest, data_frame['seats']. We then use the groupby
+#method, a function which *groups* the 'seats' column *by* the 
+#values in the 'party' column. If we then call the mean method on 
+#this newly grouped data, Python will return the average seats
+#for each party. The same syntax works equally well with the std
+#method, which returns the standard deviation of the mean we just
+#calculated. What does this tell us about the respective parties?
+
+def a_function(x):
+    if x > 1:
+        print("x is greater than 1")
+    else:
+        print("x is less than or equal to 1")
+
+a_function(5)
+a_function(-5)
+
+#This example of a simple function demonstrates one of the most 
+#important aspects of Python as a programming language: indentation.
+#Whereas in R indentation is of little importance (it is mainly used
+#as a convention to help with readability of code), in Python it is
+#extremely important. Indentation tells Python which sections of 
+#code belong together. Here, the "if" and "else" statements are both
+#at the same level of indentation, as are the two calls to "print".
+#This is a bit like telling Python "if the argument I give you, x,
+#is bigger than 1, print the first statement. Otherwise, print the
+#second one." In R, we would use curly braces {} and commas "," to
+#separate these statements.
+
+
+##########
+# Plotting
+##########
+
+plt.plot(data_frame.year, data_frame.seats)
+plt.xlabel("Year")
+plt.ylabel("Seats")
+plt.title("No. of seat won by leading party, General Elections 1992-2020")
+
+#We use the matplotlib library in Python to provide graphical 
+#capabilities, aliased as "plt". Here, we create a simple line plot
+#of the number of seats won by the leading party at each GE since
+#1992. Note how we build up our plot in stepwise fashion: we begin
+#with a call to the plot() function, in which we specify our x and
+#y axes, taking advantage of *positional* arguments (i.e. the 
+#*first* argument in the function is the x axis, the *second* is the
+#y axis). We then call the xlabel(), ylabel() and title() functions.
+#This approach to plotting graphics is very similar to R's base
+#graphics, which we briefly used yesterday. In spyder the resulting
+#plot is displayed automatically. In other editors you may need to
+#explicitly render the plot using the plt.show() function.
+
+##########
+# Activity
+##########
+
+#By recycling the code in below (i.e. changing the values), try to 
+#create a dataframe and a line plot for the number of seats of the 
+#second largest party over the same period. You can find the figures 
+#at wikipedia: https://en.wikipedia.org/wiki/D%C3%A1il_election_results
+
+data = {'year': [1992, 1997, 2002, 2007, 2011, 2016, 2020],
+        'party': ['Fianna Fail', 'Fianna Fail', 'Fianna Fail', 'Fianna Fail', 'Fine Gael', 'Fine Gael', 'Fianna Fail'],
+        'seats': [68, 77, 81, 78, 76, 50, 38]}
+
+data_frame = pd.DataFrame(data)
+print(data_frame)
+
+data_frame.seats.mean()
+data_frame['seats'].mean()
+
+data_frame['seats'].groupby(data_frame['party']).mean()
+data_frame['seats'].groupby(data_frame['party']).std()
+
+plt.plot(data_frame.year, data_frame.seats)
+plt.xlabel("Year")
+plt.ylabel("Seats")
+plt.title("No. of seat won by leading party, General Elections 1992-2020")
+
+
+################
+# And finally...
+################
+
+#There is much more to Python as a language, but the learning curve
+#can be quite steep, and it is easy to become overwhelmed. For this
+#reason we will leave it here for today - Congratulations, you 
+#survived Python basics!
